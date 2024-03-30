9789077fd24f3101228ec9ed6eec0e549be1fc70
Anvar Alimov <anvar@Anvars-MacBook-Air.local>
2024-03-30 23:39:24 +0500

    initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 300,
+              width: 300,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/image/image.png',
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(width: 20),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Anvar Alimov',
+                  style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Software engineer from Uzbekistan',
+                  style: TextStyle(fontSize: 25),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Row(
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.xTwitter,
+                        ),
+                        Text(
+                          '@bilgakhan',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 25),
+                    Row(
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.instagram,
+                        ),
+                        Text(
+                          ' @bilgakhan',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 25),
+                    Row(
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.telegram,
+                        ),
+                        Text(
+                          ' @bilgakhan',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
